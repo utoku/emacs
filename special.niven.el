da6c252561703595f4d3b7ca8b65c4a32164fbc8
Utku Somer <utku.somer@gmail.com>
2019-09-08 11:06:50 +0300

    Additional stuff for niven.

--- special.niven.el
+++ special.niven.el
@@ -1,3 +1,6 @@
 ;;special stuff for niven, a vmware guest where i cannot use function keys
 
 (global-set-key [ (control b) ] 'buffer-menu)
+(global-set-key [ (control .) ] 'kill-this-buffer)
+
+(setq visible-bell 1)
