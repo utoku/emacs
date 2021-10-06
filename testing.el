3844788d4cb22c4fb130391969199d8c6081fac3
Utku Somer <utku.somer@gmail.com>
2021-10-06 09:45:46 +0300

    Increasing agenda lines for dashboard mode.

--- testing.el
+++ testing.el
@@ -31,7 +31,12 @@
   :ensure t
   :config
   (dashboard-setup-startup-hook)
-  (setq dashboard-set-footer nil))
+  (setq dashboard-set-footer nil)
+  (setq dashboard-items '((recents . 5)
+                          (bookmarks . 5)
+                          (projects . 5)
+                          (agenda .  10)
+                          (registers . 5)) )
 
 
 ;;;;circadian. theme support with time. 
